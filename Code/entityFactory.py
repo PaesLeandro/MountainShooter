e8a450cc993a73d44370070b198d88eddb656428
Leandro <leandromkt.tr@gmail.com>
2026-02-17 19:51:40 -0300

    Creating menu interation Creating ABC class Creating factory Level 1 Bg implementation: parallax

--- Code/entityFactory.py
+++ Code/entityFactory.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from Code.Const import WIN_WIDTH
+from Code.background import Background
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entity(self, entity_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: str, position=(0,0)):
+        match entity_name:
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f'Level1Bg{1}', (0,0)))
+                    list_bg.append(Background(f'Level1Bg{1}', (WIN_WIDTH, 0)))
+                return list_bg
--- Code/game.py
+++ Code/game.py
@@ -1,5 +1,7 @@
 import pygame
-from Code.Const import WIN_HEIGHT, WIN_WIDTH
+
+from Code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
+from Code.level import Level
 from Code.menu import Menu
 
 class Game:
@@ -12,6 +14,14 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window,  'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit() # Fecha a janela
+                quit() # Encerra o jogo
+            else:
+                pass
 
