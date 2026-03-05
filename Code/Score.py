7bcf2c9a3cb1670322f22a7fc2c4bb5252994150
Leandro <leandromkt.tr@gmail.com>
2026-03-05 08:03:21 -0300

    Creating README.md

--- Code/Score.py
+++ Code/Score.py
@@ -37,6 +37,8 @@ class Score:
                 else:
                     score = player_score[1]
                     text = 'Enter Player 2 name (4 characters):'
+
+            score = int(round(score))
             self.score_text(20, text, C_WHITE, SCORE_POS['EnterName'])
 
             for event in pygame.event.get():
@@ -45,7 +47,8 @@ class Score:
                     sys.exit()
                 elif event.type == KEYDOWN:
                     if event.key == K_RETURN and len(name) == 4:
-                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        db_proxy.save(
+                            {'name': name, 'score': score, 'date': get_formatted_date()})
                         self.show()
                         return
                     elif event.key == K_BACKSPACE:
@@ -62,13 +65,15 @@ class Score:
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
         self.score_text(48, 'TOP 10 SCORE', C_YELLOW, SCORE_POS['Title'])
-        self.score_text(20, 'NAME     SCORE           DATE      ', C_YELLOW, SCORE_POS['Label'])
+        self.score_text(20, 'NAME     SCORE           DATE      ',
+                        C_YELLOW, SCORE_POS['Label'])
         db_proxy = DBProxy('DBScore')
         list_score = db_proxy.retrieve_top10()
         db_proxy.close()
 
         for player_score in list_score:
             id_, name, score, date = player_score
+            score = int(round(float(score)))
             self.score_text(20, f'{name}     {score:05d}     {date}', C_YELLOW,
                             SCORE_POS[list_score.index(player_score)])
         while True:
@@ -82,8 +87,10 @@ class Score:
             pygame.display.flip()
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_font: Font = pygame.font.SysFont(
+            name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(
+            text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
 
